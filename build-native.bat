5d6dc0dc57e20ecbf74c9536c2ee4e488924ac23
David Lechner <david@lechnology.com>
2017-08-26 13:53:37 -0500

    New windows helper.exe

--- build-native.bat
+++ build-native.bat
@@ -8,5 +8,6 @@ rem choco install upx
 
 set out=native\win32\helper.exe
 
+if not exist native\win32 mkdir native\win32
 browserify --node --exclude weak out/src/helper.js | nexe --output %out% --vcBuild ia32
 upx %out%
